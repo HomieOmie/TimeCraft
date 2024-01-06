6a29523a0291c05c5e244abb8f903b8f83284273
Om Patel <opatel@andrew.cmu.edu>
2024-01-06 16:17:31 -0500

    Full backend is done for now; now wokring on react components

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,33 +1,22 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json({msg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
-router.get('/:id', (req, res) => {
-    res.json({msg: 'GET single workout'})
-})
+router.get('/:id', getWorkout)
 
-router.post('/', async (req, res) => {
-    const {title, load, reps} = req.body
+router.post('/', createWorkout)
 
-    try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.delete('/:id', deleteWorkout)
 
-router.delete('/:id', (req, res) => {
-    res.json({msg: 'DELETE a new workout'})
-})
-
-router.patch('/:id', (req, res) => {
-    res.json({msg: 'UPDATE a new workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
